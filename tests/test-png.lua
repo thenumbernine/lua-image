9a5500c0b0d1ecf603ff00f55301946b9bba8403
Christopher Moore <christopher.e.moore@gmail.com>
2024-10-15 13:57:58 +0600

    changes to tests

--- tests/test-png.lua
+++ tests/test-png.lua
@@ -5,7 +5,13 @@ local image = Image(... or 'test.png')
 print(require 'ext.tolua'(image))
 
 image.unknown = image.unknown or {}
-image.unknown.BlEh = {
+--[[
+http://www.libpng.org/pub/png/spec/1.2/PNG-Encoders.html#E.Use-of-private-chunks
+"Applications can use PNG private chunks to carry information that need not be understood by other applications.
+	Such chunks must be given names with **lowercase second letters**, to ensure that they can never conflict with any future public chunk definition. "
+"Use an ancillary chunk type (**lowercase first letter**), not a critical chunk type, for all private chunks that store information that is not absolutely essential to view the image. "
+--]]
+image.unknown.blEH = {
 	data = 'Testing Testing',
 }
 print'saving'
@@ -14,4 +20,4 @@ print'saved'
 print'loading'
 local image2 = Image'test-save.png'
 print(require 'ext.tolua'(image2))
-asserteq(image2.unknown.BlEh.data, 'Testing Testing')
+asserteq(image2.unknown.blEH.data, 'Testing Testing')
--- tests/test.lua
+++ tests/test.lua
@@ -2,10 +2,12 @@
 local path = require 'ext.path'
 local readFilename, writeFilename = ...
 local Image = require 'image'
-assert(readFilename and writeFilename, "expected test.lua <infile> <outfile>")
+assert(readFilename, "expected test.lua <infile> [<outfile>]")
 print('reading '..readFilename)
 local image = assert(Image(readFilename), "failed to open image "..readFilename)
 print(require 'ext.tolua'(image))
 
-print('writing '..writeFilename)
-assert(image:save(writeFilename), "failed to save image "..writeFilename)
+if writeFilename then
+	print('writing '..writeFilename)
+	assert(image:save(writeFilename), "failed to save image "..writeFilename)
+end
